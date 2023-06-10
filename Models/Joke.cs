dd79d04470e802a3e6510eeca03b460471570794
hih <james_super1414@hotmail.co.uk>
2023-06-10 14:13:43 +0100

    Create Joke model

---
+++ Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesApp.Models
+{
+    public class Joke
+    {
+        public Joke()
+        {
+            
+        }
+
+        public int ID { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+    }
+}
